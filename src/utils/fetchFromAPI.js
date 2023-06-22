0736282ab001370aee4cff70c87bef7fa87c3a0a
Mohammad Khalid <blykhalid1999@gmail.com>
2023-06-22 17:54:22 +0530

    initial commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '9424600a7fmshcdbbbe970adde18p1b978fjsnaabc02f31ede',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
+
+// ,
